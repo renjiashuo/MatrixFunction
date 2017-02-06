3d7f15b50f3603d8b8853d6981364b695c49ad0c
renjiashuo <renjiashuo1993@qq.com>
2017-02-06 22:57:57 +0800

    常用数学函数向量版本的C++实现

--- MatrixFunction/vectormath-impl.h
+++ MatrixFunction/vectormath-impl.h
@@ -24,7 +24,7 @@
 
 
 /*****************************************************************************
-*                               vector-impl.h
+*                               vectormath-impl.h
 *
 * Implementation for Vector math functions.
 *
