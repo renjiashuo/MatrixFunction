f55d73efd0e1441c94ecbabfb46bd09d9627c305
renjiashuo <renjiashuo1993@qq.com>
2017-02-08 13:56:14 +0800

    MatrixFunction

--- MatrixFunction/main_test.cpp
+++ MatrixFunction/main_test.cpp
@@ -38,37 +38,68 @@ int linequs1_test();
 int main()
 {
 	vector_test();// 一、C++向量类模板（支持实数向量与复数向量的各种运算）
-	matrix_test();// 二、C++矩阵类模板（支持实数矩阵与复数矩阵的各种运算）
-	vectormath_test();// 三、常用数学函数向量版本的C++实现
-	matrixmath_test();// 四、常用数学函数矩阵版本的C++实现
-	cholesky_test();// 五、实数矩阵Cholesky分解算法的C++实现
-	ccholesky_test();// 六、复数矩阵Cholesky分解算法的C++实现
-	lud_test();// 七、实数矩阵与复数矩阵LU分解算法的C++实现
-	qrd_test();// 八、实数矩阵QR分解算法的C++实现
-	cqrd_test();// 九、复数矩阵QR分解算法的C++实现
-	svd_test();// 十、实数矩阵SVD分解算法的C++实现
-	csvd_test();// 十一、复数矩阵SVD分解算法的C++实现
-	evd_test();// 十二、实数矩阵特征值分解算法C++实现
-	cevd_test();// 十三、复数矩阵特征值分解算法的C++实现
-	inverse_test();// 十四、各种实数矩阵与复数矩阵求逆算法的C++实现
-	pseudoinverse_test();// 十五、实数矩阵与复数矩阵伪逆算法的C++实现
-	linequs1_test();// 十六、各种线性方程组求解算法C++实现（支持复系数方程组）
-	linequs2_test();// 十七、最小二乘解与极小范数解算法的C++实现（支持复系数方程组）
-	linequs3_test();// 十八、病态方程组求解方法的C++实现（支持复系数方程组）
-	nleroot_test();// 十九、非线性方程求根算法的C++实现
-	nleroots_test();// 二十、非线性方程组求解算法的C++实现
-	integral_test();// 二十一、Romberg数值积分算法的C++实现
-	newtoninterp_test();// 二十二、Newton插值算法的C++实现
-	spline3interp_test();// 二十三、三次样条插值算法的C++实现
-	lsfit_test();// 二十四、最小二乘曲线拟合算法的C++实现
-	steepdesc_test();// 二十五、最速下降优化算法的C++实现
-	conjgrad_test();// 二十六、共轭梯度优化算法的C++实现
-	utilities_test();// 二十七、信号处理算法中常用辅助函数的C++实现
-	fft_test();// 二十八、类似Matlab中FFT函数调用形式的C++实现
-	fftmr_test();// 二十九、2的整次幂长度FFT算法的C++实现
-	fftpf_test();// 三十、任意长度FFT算法的C++实现
-	bfgs_test();// 三十一、BFGS优化算法的C++实现
-
 	system("pause");
+	matrix_test();// 二、C++矩阵类模板（支持实数矩阵与复数矩阵的各种运算）
+	system("pause");
+	vectormath_test();// 三、常用数学函数向量版本的C++实现
+	system("pause");
+	matrixmath_test();// 四、常用数学函数矩阵版本的C++实现
+	system("pause");
+	cholesky_test();// 五、实数矩阵Cholesky分解算法的C++实现
+	system("pause");
+	ccholesky_test();// 六、复数矩阵Cholesky分解算法的C++实现
+	system("pause");
+	lud_test();// 七、实数矩阵与复数矩阵LU分解算法的C++实现
+	system("pause");
+	qrd_test();// 八、实数矩阵QR分解算法的C++实现
+	system("pause");
+	cqrd_test();// 九、复数矩阵QR分解算法的C++实现
+	system("pause");
+	svd_test();// 十、实数矩阵SVD分解算法的C++实现
+	system("pause");
+	csvd_test();// 十一、复数矩阵SVD分解算法的C++实现
+	system("pause");
+	evd_test();// 十二、实数矩阵特征值分解算法C++实现
+	system("pause");
+	cevd_test();// 十三、复数矩阵特征值分解算法的C++实现
+	system("pause");
+	inverse_test();// 十四、各种实数矩阵与复数矩阵求逆算法的C++实现
+	system("pause");
+	pseudoinverse_test();// 十五、实数矩阵与复数矩阵伪逆算法的C++实现
+	system("pause");
+	linequs1_test();// 十六、各种线性方程组求解算法C++实现（支持复系数方程组）
+	system("pause");
+	linequs2_test();// 十七、最小二乘解与极小范数解算法的C++实现（支持复系数方程组）
+	system("pause");
+	linequs3_test();// 十八、病态方程组求解方法的C++实现（支持复系数方程组）
+	system("pause");
+	nleroot_test();// 十九、非线性方程求根算法的C++实现
+	system("pause");
+	nleroots_test();// 二十、非线性方程组求解算法的C++实现
+	system("pause");
+	integral_test();// 二十一、Romberg数值积分算法的C++实现
+	system("pause");
+	newtoninterp_test();// 二十二、Newton插值算法的C++实现
+	system("pause");
+	spline3interp_test();// 二十三、三次样条插值算法的C++实现
+	system("pause");
+	lsfit_test();// 二十四、最小二乘曲线拟合算法的C++实现
+	system("pause");
+	steepdesc_test();// 二十五、最速下降优化算法的C++实现
+	system("pause");
+	conjgrad_test();// 二十六、共轭梯度优化算法的C++实现
+	system("pause");
+	utilities_test();// 二十七、信号处理算法中常用辅助函数的C++实现
+	system("pause");
+	fft_test();// 二十八、类似Matlab中FFT函数调用形式的C++实现
+	system("pause");
+	fftmr_test();// 二十九、2的整次幂长度FFT算法的C++实现
+	system("pause");
+	system("pause");
+	fftpf_test();// 三十、任意长度FFT算法的C++实现
+	system("pause");
+	bfgs_test();// 三十一、BFGS优化算法的C++实现
+	system("pause");
+
 	return 0;
 }
